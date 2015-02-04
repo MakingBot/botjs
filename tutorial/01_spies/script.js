c7611f5390d80027056846eded454d283c9aedb1
Xavier Rodriguez <xavier.rodriguezalvarez@gmail.com>
2015-02-04 18:46:32 +0100

    [5]Start correcting tuto 01

--- tutorial/01_spies/script.js
+++ tutorial/01_spies/script.js
@@ -1,20 +1,11 @@
-// Create shell
-block.createBlock("shell", "shell");
-
-// Create robot
-block.createBlock("robot", "bot");
-bot.name = 'name of the bot';
-
 // Create spies
-block.createBlock("spy", "agent007");
-block.createBlock("spy", "agent008");
-block.createBlock("spy", "agent009");
-block.createBlock("spy", "agent010");
+core.create("spy", "agent007");
+core.create("spy", "agent008");
+core.create("spy", "agent009");
+core.create("spy", "agent010");
 
 // Connect spies to robot
-agent007.connect(bot);
 agent008.connect(agent007);
-agent009.connect(bot);
 agent010.connect(agent007);
 
 // Display spies
@@ -22,6 +13,3 @@ agent007.show();
 agent008.show();
 agent009.show();
 agent010.show();
-
-// Start shell
-shell.start();
