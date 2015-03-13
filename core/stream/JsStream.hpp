7d1fea11a7028abf3ae8a0e1828e13337bf6ae1a
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2015-03-13 16:33:18 +0100

    Create JsStream.hpp

---
+++ core/stream/JsStream.hpp
@@ -0,0 +1,39 @@
+#ifndef JSSTREAM_HPP
+#define JSSTREAM_HPP
+//!
+//! \file JsStream.hpp
+//!
+// Copyright 2015 MakingBot
+// This file is part of BotJs.
+//
+// BotJs is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// BotJs is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with BotJs.  If not, see <http://www.gnu.org/licenses/>.
+
+//!
+//! Stream to convert the core architecture to a Js script file
+//!
+//! \author [XR]MakingBot ( http://makingbot.fr )
+//!
+class JsStream
+{
+
+public:
+
+// utilisation ideal ->  JsStream(dev) << core
+
+protected:
+
+
+};
+
+#endif // JSSTREAM_HPP
