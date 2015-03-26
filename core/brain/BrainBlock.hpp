610ef2a5fa97d59bd3242863214fb1f574f0eb53
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2015-03-26 13:25:39 +0100

    Update BrainBlock.hpp

--- core/brain/BrainBlock.hpp
+++ core/brain/BrainBlock.hpp
@@ -6,5 +6,15 @@ class BrainBlock : public BotBlock
 {
 
 
+
+    void createLoop(const QString& name, QJSValue function, int frequency);
+    
+    void createEvent(const QString& name, QJSValue condition);
+    
+    void attachEvent(const QString& name, QJSValue action);
+
+
+    void log(const QString& message);
+
 };
 
